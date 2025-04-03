2832af9bf69444f314b8fa79121b232f05881c9a
Dorian Khireddine <dorian.khireddine@etu.unilim.fr>
2025-04-03 15:20:16 +0200

    update credentials

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,12 +1,22 @@
 let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
-let Movie = {};
+let templateFile2 = await fetch("./component/Movie/templateCard.html");
+let templateCards = await templateFile2.text();
 
-Movie.format = function (hAbout, hHome) {
+let MovieCard = {};
+
+MovieCard.format = function (MovieCard) {
   let html = template;
-  html = html.replace("{{hAbout}}", hAbout);
+  let cardsHTML="";
+  for(let c of obj){
+    let card = templateCards;
+    card = card.replace("{{image}}", c.image);
+    card = card.replace("{{name}}", c.name);
+    cardHTML+=card;
+  }
+  html=html.replace("{{cards}}", cardsHTML);
   return html;
 };
 
-export { Movie };
+export { MovieCard };
